26be56e09a36f424821409f53a7ea58ff7157304
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-08-11 14:59:14 -0300

    inputRef

--- ListadeCompras/src/App.jsx
+++ ListadeCompras/src/App.jsx
@@ -1,14 +1,20 @@
-import React from "react"
+import { useRef } from "react"
 
 function App() {
+  const inputRef = useRef ()
+
+  function cliqueinoBotao(){
+    console.log(inputRef.current.value)
+  }
+
   return (
     <div>
       <h1>Lista de Compras</h1>
-      <input/>
-      <button>Adicionar Produto</button>
+      <h2>teste</h2>
+      <input placeholder="Produtos" ref={inputRef}/>
+      <button onClick={cliqueinoBotao}>Adicionar Produto</button>
     </div>
   )
 }
-
 
 export default App
